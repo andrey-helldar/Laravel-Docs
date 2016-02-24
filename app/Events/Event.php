856fdae9cdc7563a88f4d7ede2adb08952e84bec
Andrey Helldar <helldar@ai-rus.com>
2016-02-24 20:22:27 +0800

    First Commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaraDoc\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaraDoc\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
