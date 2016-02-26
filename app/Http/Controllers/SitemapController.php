a31eeb72bab3fdb9a19535566ee8fda5c57a5c9b
Andrey Helldar <helldar@ai-rus.com>
2016-02-26 23:39:00 +0800

    Fixed uses in SitemapController

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,9 +2,13 @@
 
 namespace LaraDoc\Http\Controllers;
 
-use Illuminate\Http\Request;
-use LaraDoc\Http\Requests;
+use Illuminate\Support\Facades\Cache;
 use LaraDoc\Http\Controllers\Controller;
+use Storage;
+use function config;
+use function str_slug;
+use function url;
+use function view;
 
 class SitemapController extends Controller
 {
@@ -19,17 +23,17 @@ class SitemapController extends Controller
         $url = explode("/", \Request::getUri());
 
         return Cache::remember(str_slug($url[2].'sitemap'), config('settings.cache', 60), function() {
-                    $directories = \Storage::directories('docs');
-                    unset($resources);
+                    $directories = Storage::directories('docs');
+                    $resources   = [];
 
                     foreach ($directories as $dir) {
-                        $files = \Storage::files($dir);
+                        $files = Storage::files($dir);
 
                         if (count($files) > 0) {
                             foreach ($files as $file) {
                                 $resources[] = [
                                     'link'         => url(substr($file, 0, count($file) - 4)),
-                                    'lastModified' => date("Y-m-d", \Storage::lastModified($file)),
+                                    'lastModified' => date("Y-m-d", Storage::lastModified($file)),
                                     'priority'     => '0.5'
                                 ];
                             }
